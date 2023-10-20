2606298b73f71b20b750659dfd2776ebf38cf41d
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2023-10-20 19:45:18 +0100

    update component colours with everforest vars

--- status/application.sh
+++ status/application.sh
@@ -1,7 +1,7 @@
 show_application() {
   local index=$1
   local icon=$(get_tmux_option "@catppuccin_application_icon" "")
-  local color=$(get_tmux_option "@catppuccin_application_color" "$thm_pink")
+  local color=$(get_tmux_option "@catppuccin_application_color" "$thm_purple")
   local text=$(get_tmux_option "@catppuccin_application_text" "#W")
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
--- status/directory.sh
+++ status/directory.sh
@@ -1,7 +1,7 @@
 show_directory() {
   local index=$1
   local icon=$(get_tmux_option "@catppuccin_directory_icon" "")
-  local color=$(get_tmux_option "@catppuccin_directory_color" "$thm_pink")
+  local color=$(get_tmux_option "@catppuccin_directory_color" "$thm_purple")
   local text=$(get_tmux_option "@catppuccin_directory_text" "#{b:pane_current_path}")
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
