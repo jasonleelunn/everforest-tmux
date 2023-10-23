e813f0110627b8c25f5a761ea0fda01e2c01bec9
Jason Lunn <61010854+jasonleelunn@users.noreply.github.com>
2023-10-24 00:51:37 +0100

    switch window modules to show app instead of cwd

--- window/window_current_format.sh
+++ window/window_current_format.sh
@@ -2,7 +2,7 @@ show_window_current_format() {
   local number="#I"
   local color="$thm_orange"
   local background="$thm_bg_dim"
-  local text="$(get_tmux_option "@catppuccin_window_current_text" "#{b:pane_current_path}")" # use #W for application instead of directory
+  local text="$(get_tmux_option "@catppuccin_window_current_text" "#W")"
   local fill="$(get_tmux_option "@catppuccin_window_current_fill" "number")" # number, all, none
 
   local current_window_format=$( build_window_format "$number" "$color" "$background" "$text" "$fill" )
